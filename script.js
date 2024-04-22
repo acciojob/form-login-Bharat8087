39736585834ca164859b21aa47216e547b98c6b3
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-22 13:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function getFormvalue() {
     //Write your code here
 
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+
+    if (firstName !== "" && lastName !== "") {
+        var fullName = firstName + " " + lastName;
+        alert("First and Last Name: " + fullName);
+    } else {
+        alert("Please fill in both First Name and Last Name fields.");
+    }
+}
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    getFormvalue();
+});
+
 }
